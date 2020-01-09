0d4e338eeb713ef471b29c60bf93af5149d4c761
hrhw <55056538+yangzhen970210@users.noreply.github.com>
2020-01-09 17:17:21 +0800

    Update _flask.py

--- _flask.py
+++ _flask.py
@@ -1,6 +1,16 @@
 # 20200109 
+
+# 2020010901
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+  return '<h1>hello world<h1>'
+
+@app.route('/user/<name>')
+def uesr(name):
+  return '<h1>hello, %s</h1>' % name
+
+if __name__ == '__main__':
+  app.run(debug=True)
